692448d97fdbb1e545fcc1fe393d543f30f8ba73
Yashwant Yadav <yadavyashwant.278@gmail.com>
2022-04-06 16:45:51 +0530

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Defines the root path route ("/")
+  
+  root 'friends#index'
+  get 'home/about'
+end
